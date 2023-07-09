f42072dbe39b7ddcf45b68f03bd464508308a8df
Huguinlr <huguin_lr@hotmail.com>
2023-07-09 19:04:06 +0100

    Exerceicio calculo idade

--- CURSOJS/aula12ex/ex015/script.js
+++ CURSOJS/aula12ex/ex015/script.js
@@ -8,7 +8,44 @@ function verificar () {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: <strong>${idade}</strong>`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade <10 ){
+                // Criança
+                img.setAttribute('src','foto-crianca-m.png')
+        } else if (idade < 20) {
+                // Jovem
+                img.setAttribute('src','foto-jovem-m.png')
+        } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src','foto-adulto-m.png')
+        } else {
+                // Idoso
+                img.setAttribute('src','foto-idoso-m.png')
+        }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0  && idade < 10) {
+                // Criança
+                img.setAttribute('src','foto-crianca-f.png')
+        } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src','foto-jovem-f.png')
+        } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src','foto-adulto-f.png')
+        } else {
+                // Idoso
+                img.setAttribute('src','foto-idoso-f.png')
+        }
+
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML= `Detectamos ${genero} com ${idade}`
+        res.appendChild(img)
     }
 
 } 
